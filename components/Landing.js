c8409b2c9d2d34db05885fab1a0239d8e3687b87
TarunJCole <elocnurat@hotmail.com>
2019-11-12 16:04:01 +0000

    landing

---
+++ components/Landing.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import theme from "../theme/theme";
+
+const Landing = () => {
+	return (
+		<section>
+			<h1>Landing</h1>
+		</section>
+	);
+};
+
+export default Landing;

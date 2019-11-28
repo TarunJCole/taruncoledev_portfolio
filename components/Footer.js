b774828c9c62014bc526a3d923b864dc36c92df9
TarunJCole <elocnurat@hotmail.com>
2019-11-28 13:37:27 +0000

    footer icons

--- components/Footer.js
+++ components/Footer.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { IconContext } from "react-icons";
+import { FaReact, FaNodeJs } from "react-icons/fa";
 
 import theme from "../theme/theme";
 
@@ -9,7 +11,14 @@ const Footer = () => {
 				<a href="mailto:tarun@tcwebservices.co.uk">tarun@tcwebservices.co.uk</a>
 			</div>
 			<div>&copy; tcwebservices 2019-2020</div>
-			<div></div>
+			<div>
+				<span>
+					Made with
+					<img src="./reactjs-icon.svg" alt="React Icon" className="icon" />
+					<img src="./nodejs-icon.svg" alt="Node Icon" className="icon" />
+					<img src="./expressjs-icon.svg" alt="Express Icon" className="icon" />
+				</span>
+			</div>
 			<style jsx>
 				{`
 					footer {
@@ -26,6 +35,11 @@ const Footer = () => {
 						align-items: center;
 						justify-content: center;
 						padding: 3rem 2rem;
+					}
+
+					img.icon {
+						width: 24px;
+						height: 24px;
 					}
 				`}
 			</style>

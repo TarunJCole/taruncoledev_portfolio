7a3508b241f46baf888dd7d24479a9bebdeae8ed
TarunJCole <elocnurat@hotmail.com>
2020-02-06 15:42:39 +0000

    contact icons

--- pages/contact.js
+++ pages/contact.js
@@ -8,13 +8,52 @@ const Contact = () => {
 		<Layout title="Contact">
 			<div className="page">
 				<div className="container">
-					<h1>Want To Talk?</h1>
+					<h1>
+						Want To <span className="primary">Talk?</span>
+						<br />
+						Get in touch with me at
+					</h1>
+					<div>
+						<h2>Tarun@tcsites.co.uk</h2>
+					</div>
+					<div>
+						<ul>
+							<li>
+								<a href="https://twitter.com/" aria-label="Twitter">
+									<img src="./twitter-official.svg" alt="Twitter Icon" id="twitter-icon" />
+								</a>
+							</li>
+							<li>
+								<a href="https://github.com/TarunJCole?tab=repositories" aria-label="Github">
+									<img src="./github-tile.svg" alt="Github Icon" id="github-icon" />
+								</a>
+							</li>
+						</ul>
+					</div>
 				</div>
 			</div>
 			<style jsx>
 				{`
 					h1 {
 						margin-top: 7rem;
+					}
+
+					h3 {
+						font-size: 2rem;
+					}
+
+					ul {
+						padding-left: 0;
+						display: flex;
+					}
+
+					ul img {
+						height: 64px;
+						margin-left: 2rem;
+					}
+
+					#twitter-icon {
+						margin-left: 0;
 					}
 				`}
 			</style>
